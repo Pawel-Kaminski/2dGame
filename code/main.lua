e1982afc87813e3ae92e95a989b82105ae9adcf7
Paweł Kamiński <pwkaminski@gmail.com>
2015-03-18 01:47:27 +0100

    Zaimplementowano wczytywanie mapy

--- code/main.lua
+++ code/main.lua
@@ -1,10 +1,18 @@
 local sti = require "sti"
 
 function love.load()
+    --Load a map exported to Lua from Tiled
+    map = sti.new("assets/maps/example2")
 end
 
-function love.update()
+function love.update(dt)
+    map:update(dt)
 end
 
 function love.draw()
+    map:draw()
+end
+
+function love.resize(w, h)
+    map:resize(w, h)
 end
