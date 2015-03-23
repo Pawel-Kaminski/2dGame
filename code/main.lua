395aa2a8d5b40e9d3a3c562ec86bec7632798182
Paweł Kamiński <pwkaminski@gmail.com>
2015-03-23 01:43:29 +0100

    Dodano możliwość poruszania się po mapie oraz wyjścia z gry za pomocą Esc

--- code/main.lua
+++ code/main.lua
@@ -2,6 +2,9 @@ local sti = require "sti"
 
 function love.load()
 
+    xx = 0
+    yy = 0
+    love.keyboard.setKeyRepeat(true)
     love.window.setMode(1920, 1080, {fullscreen=true}) 
 
     -- Grab window size
@@ -18,7 +21,6 @@ function love.load()
     love.resize(windowWidth, windowHeight)
 
     -- Create a Custom Layer
-    --map:addCustomLayer("Sprite Layer", 3)
     map:addCustomLayer("Sprite Layer", 2)
 
     -- Add data to Custom Layer
@@ -26,11 +28,19 @@ function love.load()
     spriteLayer.sprites = {
         player = {
             image = love.graphics.newImage("assets/sprites/man2.png"),
-            x = 60,
+            x = xx,
             y = 60,
             r = 0,       
         }
     }
+
+    -- Update callback for Custom Layer
+    function spriteLayer:update(dt)
+        for _, sprite in pairs(self.sprites) do
+            sprite.x = xx
+            sprite.y = yy
+        end
+    end
 
     -- Draw callback for Custom Layer
     function spriteLayer:draw()
@@ -53,7 +63,7 @@ function love.draw()
     local scaleY = 1
     
     -- Translation would normally be based on a player's x/y
-    --local translateX = 0
+    --local translateX = xx
     --local translateY = 0
 
     -- Draw Range culls unnecessary tiles
@@ -65,4 +75,18 @@ end
 
 function love.resize(windowWidth, windowHeight)
     map:resize(windowWidth, windowHeight)
+end
+
+function love.keypressed(key)
+    if key == "escape" then
+        love.event.quit()
+    elseif key == "right" then
+        xx = xx + 60
+    elseif key == "left" then
+        xx = xx - 60
+    elseif key == "down" then
+        yy = yy + 60
+    elseif key == "up" then
+        yy = yy - 60
+    end
 end
