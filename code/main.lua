af13c7f51334f3f70a939bcdd993e3f72e152c71
Paweł Kamiński <pwkaminski@gmail.com>
2015-04-08 01:26:28 +0200

    Dodano podstawową obsługę wielu rozdzielczości

--- code/main.lua
+++ code/main.lua
@@ -1,3 +1,4 @@
+require "TLfres"
 sti = require "sti"
 Gamestate = require "hump.gamestate"
 require "map"
@@ -6,14 +7,18 @@ function love.load() --this function is called only once
     Gamestate.registerEvents() --this makes callbacks Gamestate.update(), Gamestate.draw(), etc. work
     love.keyboard.setKeyRepeat(true)
     love.mouse.setVisible(false)
-    love.window.setMode(1920, 1080, {fullscreen=true})
+    --love.window.setMode(0, 0, {fullscreen=true})
+    TLfres.setScreen({w=0, h=0, full=true, vsync=false, aa=16}, 1920)
     Gamestate.switch(mapState)
 end
-
-
 
 function love.keypressed(key)
     if key == "escape" then
         love.event.quit()
     end
+end
+
+function love.draw()
+    TLfres.transform()
+    TLfres.letterbox(16,9)
 end
