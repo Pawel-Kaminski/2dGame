ceada4a4df4bfce49ba8bf3af4b7cdd0a87d4f37
Paweł Kamiński <pwkaminski@gmail.com>
2015-07-11 12:03:36 +0200

    Tworzenie backendu do 1 przeciwnika w grze

--- code/enemies/thornbush.lua
+++ code/enemies/thornbush.lua
@@ -1,1 +1,47 @@
 --TODO:implement thornbush
+EnemyThornbushStatistics = {
+    --Table contains attributes which should be loaded before the fight
+    health = 30,
+    magic = 0,
+    waitingTime = 45,
+    defence = 0
+}
+
+EnemyThornbush_First = {
+    id = 1,
+    name = "Thornbush",
+    healthPoints,
+    magicPoints,
+    waitingTime,
+    remainingWaitingTime = 29,
+    defence,
+    maxHealth,
+    maxMagic
+}
+
+--this will let me create multiple instances of enemy
+EnemyThornbush_Second = {
+    id = 2,
+    name = "Thornbush 2",
+    healthPoints,
+    magicPoints,
+    waitingTime,
+    remainingWaitingTime = 37,
+    defence,
+    maxHealth,
+    maxMagic
+}
+
+EnemyThornbush_Third = {
+    id = 3,
+    name = "Thornbush 3",
+    healthPoints,
+    magicPoints,
+    waitingTime,
+    remainingWaitingTime = 5,
+    defence,
+    maxHealth,
+    maxMagic
+}
+
+--TODO:Implement attacks
