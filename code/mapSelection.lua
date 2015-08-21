a3d5c21d9526e00be5291d77c8fe6463ff2d9226
Paweł Kamiński <pwkaminski@gmail.com>
2015-08-21 20:56:43 +0200

    Zaimplementowano działanie strzałki w górę na ekranie wyboru mapy

--- code/mapSelection.lua
+++ code/mapSelection.lua
@@ -123,7 +123,27 @@ function mapSelectionState:keypressed(key)
         end
     end
     if key == "up" then
-        --TODO: implement map selection
+        if elements.sprites.wioskaLipinkiOn.active then
+            elements.sprites.wioskaLipinkiOn.active = false
+            elements.sprites.wioskaLipinkiOff.active = true
+            elements.sprites.oltarzPrzeznaczeniaOn.active = true
+            elements.sprites.oltarzPrzeznaczeniaOff.active = false
+        elseif elements.sprites.mrocznyLasOn.active then
+            elements.sprites.mrocznyLasOn.active = false
+            elements.sprites.mrocznyLasOff.active = true
+            elements.sprites.wioskaLipinkiOn.active = true
+            elements.sprites.wioskaLipinkiOff.active = false
+        elseif elements.sprites.krainaRownowagiOn.active then
+            elements.sprites.krainaRownowagiOn.active = false
+            elements.sprites.krainaRownowagiOff.active = true
+            elements.sprites.mrocznyLasOn.active = true
+            elements.sprites.mrocznyLasOff.active = false
+        elseif elements.sprites.oltarzPrzeznaczeniaOn.active then
+            elements.sprites.oltarzPrzeznaczeniaOn.active = false
+            elements.sprites.oltarzPrzeznaczeniaOff.active = true
+            elements.sprites.krainaRownowagiOn.active = true
+            elements.sprites.krainaRownowagiOff.active = false
+        end
     end
     if key == "return" then
         if elements.sprites.wioskaLipinkiOn.active then
