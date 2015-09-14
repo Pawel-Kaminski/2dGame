3770cad063ac649da633e60f2ed5fa2cffcfa765
Paweł Kamiński <pwkaminski@gmail.com>
2015-09-14 19:33:33 +0200

    Dodano wskaźnik przy wyświetlaniu listy przedmiotów

--- code/battle/drawBattle/showItems.lua
+++ code/battle/drawBattle/showItems.lua
@@ -1,9 +1,9 @@
 function showItems()
+    arrowY = 900
     displayingMenu = false
-    love.graphics.printf("Mikstura lecznicza", 150, 915, 1000, "left", 0)
-    love.graphics.printf("Eliksir leczniczy", 150, 945, 1000, "left", 0)
-    love.graphics.printf("Antidotum", 150, 975, 1000, "left", 0)
-    love.graphics.printf("Trucizna", 150, 1005, 1000, "left", 0)
-    love.graphics.printf("Eliksir magiczny", 150, 1035, 1000, "left", 0)
-    lastPositionY = 915
+    love.graphics.printf("mikstura lecznicza", 150, 915, 1000, "left", 0)
+    love.graphics.printf("eliksir leczniczy", 150, 945, 1000, "left", 0)
+    love.graphics.printf("antidotum", 150, 975, 1000, "left", 0)
+    love.graphics.printf("trucizna", 150, 1005, 1000, "left", 0)
+    love.graphics.printf("eliksir magiczny", 150, 1035, 1000, "left", 0)
 end
--- code/battle/enterBattle/spriteLayer/drawSpriteLayer.lua
+++ code/battle/enterBattle/spriteLayer/drawSpriteLayer.lua
@@ -5,7 +5,7 @@ function drawSpriteLayer(layer)
     for _, sprite in pairs(layer.sprites) do
         --if describes conditions when arrow is displayed
         if (sprite.y ~= arrowY or displayingMenu or displayingActions or defeat
-        or selectingEnemy) and sprite.active ~= false then
+        or selectingEnemy or displayingItems) and sprite.active ~= false then
             local x = math.floor(sprite.x)
             local y = math.floor(sprite.y)
             local r = sprite.r
