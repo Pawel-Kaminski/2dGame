9b4b11f1e35c988f2a603659d2c025fe2d0d3030
Paweł Kamiński <pwkaminski@gmail.com>
2015-09-20 07:48:10 +0200

    Przedmioty nieobecne w ekwipunku są wyświetlane na szaro

--- code/battle/drawBattle/showItems.lua
+++ code/battle/drawBattle/showItems.lua
@@ -1,16 +1,39 @@
 --number of item: 1-5
+--if no items are available, name of item should be displayed in gray
+--if item is available and isn't selected, name should be displayed in white
+--if it is selected, name should be displayed in red
 
 function showItems()
     displayingMenu = false
-    color(900)
+    if items[1][2] == 0 then
+        love.graphics.setColor(139, 137, 137)
+    else
+        color(900)
+    end
     love.graphics.printf("mikstura lecznicza", 150, 915, 1000, "left", 0)
-    color(930)
+    if items[2][2] == 0 then
+        love.graphics.setColor(139, 137, 137)
+    else
+        color(930)
+    end
     love.graphics.printf("eliksir leczniczy", 150, 945, 1000, "left", 0)
-    color(960)
+    if items[3][2] == 0 then
+        love.graphics.setColor(139, 137, 137)
+    else
+        color(960)
+    end
     love.graphics.printf("antidotum", 150, 975, 1000, "left", 0)
-    color(990)
+    if items[4][2] == 0 then
+        love.graphics.setColor(139, 137, 137)
+    else
+        color(990)
+    end
     love.graphics.printf("trucizna", 150, 1005, 1000, "left", 0)
-    color(1020)
+    if items[5][2] == 0 then
+        love.graphics.setColor(139, 137, 137)
+    else
+        color(1020)
+    end
     love.graphics.printf("eliksir magiczny", 150, 1035, 1000, "left", 0)
 
     love.graphics.setColor(255, 255, 255)
