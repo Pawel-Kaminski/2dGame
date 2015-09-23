afeb9de9bc1704c59ae4b141694f9e6f0b3e97f5
Paweł Kamiński <pwkaminski@gmail.com>
2015-09-23 17:17:20 +0200

    Zaimplementowano możliwość użycia mikstury leczniczej

--- code/battle/keypressedBattle/displayItems/useItem.lua
+++ code/battle/keypressedBattle/displayItems/useItem.lua
@@ -2,6 +2,10 @@ function useItem(number)
     if number == 1 and items[1][2] > 0 then
         --use health potion
         items[1][2] = items[1][2] - 1
+        Player.healthPoints = Player.healthPoints + 100
+        if Player.healthPoints > Player.maxHealth then
+            Player.healthPoints = Player.maxHealth
+        end
     elseif number == 2 and items[2][2] > 0 then
         --use health elixir
         items[2][2] = items[2][2] - 1
@@ -14,5 +18,15 @@ function useItem(number)
     elseif number == 5 and items[5][2] > 0 then
         --use magic elixir
         items[5][2] = items[5][2] - 1
+    else return
+    end
+
+    if not selectingEnemy then
+        Player.remainingWaitingTime = Player.waitingTime
+        if not victory then
+            countingActive = true
+            displayingItems = false
+            arrowY = 900
+        end
     end
 end
