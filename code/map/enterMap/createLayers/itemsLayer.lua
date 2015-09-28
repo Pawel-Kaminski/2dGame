62c99db526a069c6a59927293b2910761838a4ec
Paweł Kamiński <pwkaminski@gmail.com>
2015-09-28 23:04:59 +0200

    Wszystkie przedmioty można prawidłowo zbierać z mapy

--- code/map/enterMap/createLayers/itemsLayer.lua
+++ code/map/enterMap/createLayers/itemsLayer.lua
@@ -51,7 +51,7 @@ function createItemsLayer(mapId)
                 y = 0,
                 active = active8
             },
-            healthPotion = {
+            healthPotion2 = {
                 image = love.graphics.newImage(
                     "assets/sprites/items/1-health_potion/pt1_scaled.png"),
                 x = 480,
@@ -83,7 +83,7 @@ function createItemsLayer(mapId)
                 y = 900,
                 active = active10
             },
-            healthPotion = {
+            healthPotion3 = {
                 image = love.graphics.newImage(
                     "assets/sprites/items/1-health_potion/pt1_scaled.png"),
                 x = 360,
@@ -121,6 +121,7 @@ function createItemsLayer(mapId)
                 --local r = sprite.r
                 if playerPositionX == x and playerPositionY == y then
                     sprite.active = false
+                    --If sprite is null, it doesn't exist on this particular map
                     if itemsLayer.sprites.healthPotion ~= null and
                         x == itemsLayer.sprites.healthPotion.x and
                         y == itemsLayer.sprites.healthPotion.y then
@@ -156,6 +157,31 @@ function createItemsLayer(mapId)
                         y == itemsLayer.sprites.iceArtefact.y then
                             items[11][2] = 1
                             active10 = false
+                    elseif itemsLayer.sprites.healthPotion2 ~= null and
+                        x == itemsLayer.sprites.healthPotion2.x and
+                        y == itemsLayer.sprites.healthPotion2.y then
+                            items[1][2] = items[1][2] + 1
+                            active11 = false
+                    elseif itemsLayer.sprites.healthElixir ~= null and
+                        x == itemsLayer.sprites.healthElixir.x and
+                        y == itemsLayer.sprites.healthElixir.y then
+                            items[2][2] = items[2][2] + 1
+                            active12 = false
+                    elseif itemsLayer.sprites.healthPotion3 ~= null and
+                        x == itemsLayer.sprites.healthPotion3.x and
+                        y == itemsLayer.sprites.healthPotion3.y then
+                            items[1][2] = items[1][2] + 1
+                            active13 = false
+                    elseif itemsLayer.sprites.antidote ~= null and
+                        x == itemsLayer.sprites.antidote.x and
+                        y == itemsLayer.sprites.antidote.y then
+                            items[3][2] = items[3][2] + 1
+                            active14 = false
+                    elseif itemsLayer.sprites.manaElixir ~= null and
+                        x == itemsLayer.sprites.manaElixir.x and
+                        y == itemsLayer.sprites.manaElixir.y then
+                            items[5][2] = items[5][2] + 1
+                            active15 = false
                     end
                 end
                 love.graphics.draw(sprite.image, x, y)
