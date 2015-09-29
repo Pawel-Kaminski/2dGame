d5fab73847523a30bae59db7f4c5de5e1bf22e2b
Paweł Kamiński <pwkaminski@gmail.com>
2015-09-29 23:39:57 +0200

    Implementacja użycia eliksirów

--- code/battle/keypressedBattle/displayItems/useItem.lua
+++ code/battle/keypressedBattle/displayItems/useItem.lua
@@ -1,6 +1,7 @@
 function useItem(number)
     if number == 1 and items[1][2] > 0 then
         --use health potion
+        --recover small amount of HP
         items[1][2] = items[1][2] - 1
         Player.healthPoints = Player.healthPoints + 100
         if Player.healthPoints > Player.maxHealth then
@@ -8,16 +9,22 @@ function useItem(number)
         end
     elseif number == 2 and items[2][2] > 0 then
         --use health elixir
+        --fully recover HP
         items[2][2] = items[2][2] - 1
+        Player.healthPoints = Player.maxHealth
     elseif number == 3 and items[3][2] > 0 then
         --use antidotum
+        --remove poison effect
         items[3][2] = items[3][2] - 1
     elseif number == 4 and items[4][2] > 0 then
         --use poison
+        --inflict poison effect on enemy
         items[4][2] = items[4][2] - 1
     elseif number == 5 and items[5][2] > 0 then
         --use magic elixir
+        --fully recover MP
         items[5][2] = items[5][2] - 1
+        Player.magicPoints = Player.maxMagic
     else return
     end
 
