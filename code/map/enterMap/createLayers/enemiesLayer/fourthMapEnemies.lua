ea2f9ef2aaca242387c3d269b28d05f3220684cd
Paweł Kamiński <pwkaminski@gmail.com>
2015-10-24 23:24:51 +0200

    Dodanie bossa na ostatniej mapie

--- code/map/enterMap/createLayers/enemiesLayer/fourthMapEnemies.lua
+++ code/map/enterMap/createLayers/enemiesLayer/fourthMapEnemies.lua
@@ -1,4 +1,13 @@
 function fourthMapEnemies()
+    local boss = love.graphics.newImage(
+        "assets/sprites/enemies/10-Neil_Merzenantar/Neil.png")
     enemies.sprites = {
+        boss = {
+            image = boss,
+            x = 960,
+            y = 540,
+            active = true,
+            name = "boss"
+        },
     }
 end
