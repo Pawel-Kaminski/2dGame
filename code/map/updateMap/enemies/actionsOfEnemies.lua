ea39bc1566a871e62dac7bf414084be4d291c540
Paweł Kamiński <pwkaminski@gmail.com>
2015-11-09 04:05:51 +0100

    Uzupełnienie pliku actionsOfEnemies

--- code/map/updateMap/enemies/actionsOfEnemies.lua
+++ code/map/updateMap/enemies/actionsOfEnemies.lua
@@ -1,20 +1,82 @@
-function harmfulGas()
+function harmfulGas(attackingEnemy)
+    displayAnimation = true
+    animationId = 6
+    enemyId = attackingEnemy.id
+    Player.healthPoints = 
+        Player.healthPoints - (3 * (100 - Player.defence) / 100)
+    if Player.healthPoints < 0 then
+        Player.healthPoints = 0
+    end
 end
 
-function magicalExplosion()
+function magicalExplosion(attackingEnemy)
+    displayAnimation = true
+    animationId = 5
+    enemyId = attackingEnemy.id
+    if attackingEnemy.magicPoints < 5 then return end
+    Player.healthPoints = 
+        Player.healthPoints - (5 * (100 - Player.defence) / 100)
+    if Player.healthPoints < 0 then
+        Player.healthPoints = 0
+    end
+    attackingEnemy.magicPoints = attackingEnemy.magicPoints - 5
 end
 
-function magicalBarrier()
+function magicalBarrier(castingEnemy)
+    displayAnimation = true
+    animationId = 7
+    enemyId = castingEnemy.id
+    castingEnemy.magicPoints = castingEnemy.magicPoints - 10
+    castingEnemy.defence = castingEnemy.defence + 10
 end
 
-function healingRune()
+function healingRune(castingEnemy)
+    displayAnimation = true
+    animationId = 8
+    enemyId = castingEnemy.id
+    if castingEnemy.magicPoints < 10 then return end
+    castingEnemy.healthPoints = castingEnemy.healthPoints + 20
+    castingEnemy.magicPoints = castingEnemy.magicPoints - 10
+    if castingEnemy.healthPoints > castingEnemy.maxHealth then
+        castingEnemy.healthPoints = castingEnemy.maxHealth    
+    end
 end
 
-function fireAttack()
+function fireAttack(attackingEnemy)
+    displayAnimation = true
+    animationId = 12
+    enemyId = attackingEnemy.id
+    if attackingEnemy.magicPoints < 5 then return end
+    Player.healthPoints = 
+        Player.healthPoints - (5 * (100 - Player.defence) / 100)
+    if Player.healthPoints < 0 then
+        Player.healthPoints = 0
+    end
+    attackingEnemy.magicPoints = attackingEnemy.magicPoints - 5
 end
 
-function iceAttack()
+function iceAttack(attackingEnemy)
+    displayAnimation = true
+    animationId = 13
+    enemyId = attackingEnemy.id
+    if attackingEnemy.magicPoints < 5 then return end
+    Player.healthPoints = 
+        Player.healthPoints - (5 * (100 - Player.defence) / 100)
+    if Player.healthPoints < 0 then
+        Player.healthPoints = 0
+    end
+    attackingEnemy.magicPoints = attackingEnemy.magicPoints - 5
 end
 
-function powerOfElements()
+function powerOfElements(attackingEnemy)
+    displayAnimation = true
+    animationId = 14
+    enemyId = attackingEnemy.id
+    if attackingEnemy.magicPoints < 5 then return end
+    Player.healthPoints = 
+        Player.healthPoints - (5 * (100 - Player.defence) / 100)
+    if Player.healthPoints < 0 then
+        Player.healthPoints = 0
+    end
+    attackingEnemy.magicPoints = attackingEnemy.magicPoints - 5
 end
